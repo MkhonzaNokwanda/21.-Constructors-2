c802cd19d4fd1d15c3618694365fea423833e6ef
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-18 14:20:31 +0200

    Add project files.

---
+++ 21. Constructors 2/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Constructors_2
+{
+    internal class Car
+    {
+        public string name;
+        public string color;
+        public int year;
+
+        //Constructor
+        public Car(string aName, string aColor, int aYear)
+        {
+            name = aName;
+            color = aColor;
+            year = aYear;
+
+        }
+    }
+}
---
+++ 21. Constructors 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Constructors_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mustang","Red",2019);
+            Car car2 = new Car("Polo", "Green", 1790);
+
+
+            //freeze Console
+            Console.ReadLine();
+        }
+    }
+}
